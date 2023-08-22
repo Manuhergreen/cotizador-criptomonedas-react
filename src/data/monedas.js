984ca349f538509dca2510cac5a10ffcd7f73683
Manuhergreen <manuhernandezbi@gmail.com>
2023-08-22 22:53:36 +0200

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    {id:'USE', nombre:'Dolar de Estados unidos'},
+    {id:'MXN', nombre:'Pesos Mexicano'},
+    {id:'EUR', nombre:'Euro'},
+    {id:'GBP', nombre:'Libra Esterlina'},
+]
+
+export {
+    monedas
+}
